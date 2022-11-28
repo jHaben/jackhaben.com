8cb7b22b584035613894b9a11f62ad98b67e9095
Jack <jack@jack-hp.Home>
2022-11-27 20:59:01 -0600

    update welcome section

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,86 +1,63 @@
 <script setup>
 import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+
 </script>
 
 <template>
   <WelcomeItem>
     <template #icon>
-      <DocumentationIcon />
+      <font-awesome-icon icon="fa-brands fa-vuejs" size="xl" />
     </template>
-    <template #heading>Documentation</template>
+    <template #heading>Vue.js</template>
+    Vue is well documented. The
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>.
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <ToolingIcon />
+      <font-awesome-icon icon="fa-brands fa-js" size="xl" />
     </template>
-    <template #heading>Tooling</template>
+    <template #heading>JS Calculator</template>
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
+    A JavaScript based <a href="https://tsg121.org/donut" target="_blank" rel="noopener">Calculator</a> I made for
+    a fundraiser.
 
-    <br />
-
-    More instructions are available in <code>README.md</code>.
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <EcosystemIcon />
+      <font-awesome-icon icon="fa-brands fa-aws" size="xl" />
     </template>
-    <template #heading>Ecosystem</template>
+    <template #heading>Cloud</template>
+    <ul>
+      <li> <a href="https://aws.amazon.com/elasticbeanstalk/" target="_blank" rel="noopener">Elastic Beanstalk</a></li>
+      <li> <a href="https://aws.amazon.com/cloudfront/" target="_blank" rel="noopener">CloudFront</a></li>
+      <li><a
+          href="https://aws.amazon.com/pm/ec2/?trk=36c6da98-7b20-48fa-8225-4784bced9843&sc_channel=ps&s_kwcid=AL!4422!3!467723097970!e!!g!!aws%20ec2&ef_id=Cj0KCQiAsoycBhC6ARIsAPPbeLtEDTRMxztEhXdOefalOvWXCmhRueydIXJXOGv9eHRa2Az1Ap1VJgkaAn2nEALw_wcB:G:s&s_kwcid=AL!4422!3!467723097970!e!!g!!aws%20ec2"
+          target="_blank" rel="noopener">EC2</a></li>
 
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
+    </ul>
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <CommunityIcon />
+      <font-awesome-icon icon="fa-brands fa-java" size="xl" />
     </template>
-    <template #heading>Community</template>
+    <template #heading>JAVA</template>
 
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
+    Spring Boot
+    
+
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <SupportIcon />
+      <font-awesome-icon icon="fa-solid fa-database" />
     </template>
-    <template #heading>Support Vue</template>
+    <template #heading>Databases</template>
 
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+    Cloud Hosted MySQL 
   </WelcomeItem>
 </template>
--- src/main.js
+++ src/main.js
@@ -9,14 +9,14 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faDatabase } from '@fortawesome/free-solid-svg-icons'
 
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithub, faAws, faVuejs, faJava, faJs} from '@fortawesome/free-brands-svg-icons'
 
 import './assets/main.css'
 
 /* add icons to the library */
-library.add(faEnvelope, faLinkedin, faGithub)
+library.add(faEnvelope, faLinkedin, faGithub, faAws, faVuejs, faJava, faJs , faDatabase)
 
 
 createApp(App)
